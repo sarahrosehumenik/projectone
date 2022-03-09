1af3abdd42393a71b29e3019427e1285d2e167ca
sarahrosehumenik <sarahrosehumenik@gmail.com>
2022-03-08 18:04:04 -0700

    intial commit

---
+++ main.js
@@ -0,0 +1,177 @@
+const buttonArray = document.getElementById('keyboard');
+console.log(buttonArray)
+
+
+
+//WORDS RELATING TO GEN Z
+
+let words = ["STAN", "CRINGE", "SIMP", "SHEEEESH", "BUSSIN", "SNATCHED", "CLAPBACK", "SKRRT", "PERIODT", "FLEX", "SUS", "SHOOK", "RECIEPTS", "SLAPS", "SHADE", "EXTRA", "GUCCI", "LIT", "RATCHET", "SAVAGE", "THIRSTY", "UNALIVE", "STORYTIME", "USSY", "BRUH","STUDIOUS", "NEUROTYPICALS", "WOMBLANDS", "GRWM", "CHUBBABUN", "THICC"]
+
+//this generates a random word from the words array
+let randomWord =
+words[Math.floor(words.length * Math.random())];
+console.log(randomWord)
+
+//this makes the random word chosen into an array
+let randomArray = randomWord.split('')
+
+
+
+
+//create spaces on the baord that equal the length of the randomword 
+
+  
+for (let i = 0; i < randomArray.length; i++) {
+  randomArray[i] = "_";
+  
+}
+  console.log(randomArray)
+
+//creating element to put prompt
+let outcome = document.getElementById('outcome')
+
+
+
+
+
+
+
+//this displays the spaces on the website
+let spaces = document.querySelector('p')
+
+spaces.innerHTML = randomArray.join(' ')
+
+
+
+  
+
+
+
+
+  
+
+
+let tries = 10 
+
+let guess = buttonArray.addEventListener('click', event) 
+  function event(evt) {
+  if (randomWord.includes(evt.target.textContent)) {
+
+    for (let i = 0; i < randomWord.length; i++){
+      if(evt.target.textContent === randomWord[i]){
+    randomArray[i] = evt.target.textContent
+        spaces.innerHTML = randomArray.join(' ')
+        console.log(randomArray)
+        
+console.log(evt.target)
+
+     
+
+    
+       
+     
+      
+      }
+    }
+
+
+  
+     outcome.innerHTML= "Thats whats up";
+     evt.target.style.backgroundColor = "#86FEB6";
+     evt.target.disabled = "true"
+    
+    } else {
+      tries -= 1; 
+      outcome.innerHTML = "you have " + tries + " tries left";
+      evt.target.style.backgroundColor = "red"
+      evt.target.disabled = "true"
+    if(tries === 0){
+      
+      outcome.innerHTML = "Its the we are all going to die for me"
+      outcome.style.color = "#55FE01"
+      buttonArray.removeEventListener('click', event )
+      
+    
+    }
+     
+      
+     
+    } 
+      
+   if(!randomArray.includes("_") ) {
+     outcome.innerHTML = "ITS THE SAVING MANKIND FOR ME"
+     outcome.style.color = "#FE0199"
+     outcome.style.fontSize = "35px"
+      buttonArray.removeEventListener('click', event )
+   }
+  
+    }
+
+
+
+// let guess = buttonArray.addEventListener('click', function(evt) {
+// console.log(evt)
+// if (evt.target.textContent === randomWord[0]){
+//   console.log(true)
+// }else {
+//   console.log(false)
+// }
+//   })
+
+
+
+
+// for (let i = 0; i < randomWord.length; i++){
+// // if(evt.target.textContent === randomWord[i]){
+// // console.log(prompt.innerHTML = "killing it");
+// // }else if (evt.target.textContent !== randomWord[i]) {
+// // console.log(prompt.innerHTML = "Killing Us");
+// // }
+// // }
+// // })
+
+
+// }// randomWord.includes(evt.target.textContent) 
+// //   console.log(true)
+                            
+
+
+// let spacesp = document.querySelector('p');
+
+// spacesp.innerHtml = spaces.appendChild('body')
+
+// console.log(spacesp)
+
+
+
+
+
+  
+
+  
+
+
+//this is a spin on hangman- same game-but altered user experience
+
+//it needs to:
+
+//have a cache of different words within an array-how many words? is there a plug in that offers words and hints already so i have a lardge database of words?
+//for each word there is a category that is displayed for the user to see when guessing
+//an icebox item would be adding a hint per word
+
+//when you hit the start button it loads the objective prompt and the blank slots for the randomized word.
+//an icebox item would be to have the animated carrot at the end of the typewritter effect that the prompt does when pressing start
+
+
+//when clicking a letter on the keyboard it will process if that letter is in the word and input in the blank if it is.
+
+// if it wrong it will place an X in the scoreboard
+//you have 9 trys to guess the "password"
+//once you have missed 9 tries the prompt changes to (take cover the astroid is close or something like that)
+
+//if you guess the word before 9 tries the prompt will change to something like 
+//"thank you for saving our planet"
+
+//if you would like to play again- hit the reset button. and then hit start. 
+
+//i need to figure out how to attach the buttons to the dom so that each letter pressed can me itterated throughout the letters of the word- and return whereever the letters are on the board in the proper blank spaces or return false and be one turn down.
